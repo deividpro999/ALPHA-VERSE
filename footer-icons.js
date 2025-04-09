c08f10f9b410f3d5c42461012b5e3013e601d4ae
deividpro999 <102329898+deividpro999@users.noreply.github.com>
2025-04-09 15:23:02 -0300

    Delete footer-icons.js

--- footer-icons.js
+++
@@ -1,9 +0,0 @@
-const footer = document.getElementById("footer-bar");
-
-if (footer) {
-  footer.innerHTML = `
-    <i title="Início">??</i>
-    <i title="Live" style="color: red;">??</i>
-    <i title="Perfil">??</i>
-  `;
-}
