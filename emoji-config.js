c4f286b0c4c3c7ca9469715d87889f8ced20a2fc
deividpro999 <102329898+deividpro999@users.noreply.github.com>
2025-04-09 15:22:47 -0300

    Delete emoji-config.js

--- emoji-config.js
+++
@@ -1,6 +0,0 @@
-// js/emoji-config.js
-const emojis = ['??', '??', '??'];
-function renderEmojis() {
-  const area = document.getElementById('emoji-area');
-  area.textContent = emojis.join(' ');
-}
